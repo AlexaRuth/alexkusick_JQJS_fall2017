51e05783890aa97bc44e9f85ef5f09d4a6423267
AlexaRuth <akusick@mcad.edu>
2017-09-24 20:21:55 -0500

    objects for speeches

--- week4/script.js
+++ week4/script.js
@@ -13,3 +13,25 @@ document.getElementById('BtnGhandi').addEventListener('click', function(){
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Demosthenes" button.
 });
+
+///objects for each speech///
+
+var churchhillSpeech={
+    author:'Winston Churchill',
+    year:   1940,
+    BCE:    false,
+}
+
+var ghandiSpeech={
+    author:'Ghandi',
+    year:   1942,
+    BCE:    false,
+}
+
+var demosthenesSpeech={
+    author:'Demosthenes',
+    year:   342,
+    BCE:    true,
+}
+
+///an array for the speeches///
