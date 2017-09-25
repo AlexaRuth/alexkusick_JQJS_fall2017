9837cbf06a193b25308aeaff9ae917aa259048c0
AlexaRuth <akusick@mcad.edu>
2017-09-24 20:33:57 -0500

    adding to bottons
    Event listener details

--- week4/script.js
+++ week4/script.js
@@ -1,5 +1,7 @@
+///event listeners///
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
+  console.log("there are" + allspeeches.length+ "speeches on this page."); //Code in here executes when the user clicks the "Donate" button.
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
@@ -20,18 +22,36 @@ var churchhillSpeech={
     author:'Winston Churchill',
     year:   1940,
     BCE:    false,
-}
+    }
 
 var ghandiSpeech={
     author:'Ghandi',
     year:   1942,
     BCE:    false,
-}
+    }
 
 var demosthenesSpeech={
     author:'Demosthenes',
     year:   342,
     BCE:    true,
-}
+    }
 
 ///an array for the speeches///
+
+var allSpeeches=[
+    churchillSpeech,
+    ghandiSpeech,
+    demosthenesSpeech
+    ];
+
+///variable calculation///
+
+var difference= 
+    ghandiSpeech.year - 
+    churchillSpeech.year;
+
+///Console log statment ///
+console.log("Ghandi's & Churchill's speeches are " + difference + "years apart.");
+
+
+
