8b0fc3215d0468430a266794c13dcde7a7da3b8e
AlexaRuth <akusick@mcad.edu>
2017-09-24 21:02:57 -0500

    updates

--- week4/script.js
+++ week4/script.js
@@ -1,57 +1,70 @@
-///event listeners///
+////event listeners////
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  console.log("there are" + allspeeches.length+ "speeches on this page."); //Code in here executes when the user clicks the "Donate" button.
-});
+  console.log("There are " 
+  	+allSpeeches.length +" speeches on this page.");
+	});
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Churchill" button.
-});
+  console.log("This speech was written by "
+	+allSpeeches[0].author +"in" 
+	+allSpeeches[0].year +". It is " 
+	+allSpeeches[0].BCE +"that this year is B.C.E.");
+	});
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Ghandi" button.
-});
+  console.log("This speech was written by " 
+  	+allSpeeches[1].author +"in" 
+  	+allSpeeches[1].year +". It is " 
+  	+allSpeeches[1].BCE +"that this year is B.C.E.");
+	});
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Demosthenes" button.
-});
+  console.log("This speech was written by " 
+  	+ allSpeeches[2].author +"in" 
+  	+ allSpeeches[2].year + ". It is " 
+  	+ allSpeeches[2].BCE + " that this year is B.C.E.");
+	});
 
-///objects for each speech///
 
-var churchhillSpeech={
+////objects for each speech////
+
+var churchillSpeech ={
     author:'Winston Churchill',
     year:   1940,
     BCE:    false,
     }
 
-var ghandiSpeech={
+var ghandiSpeech ={
     author:'Ghandi',
     year:   1942,
     BCE:    false,
     }
 
-var demosthenesSpeech={
+var demosthenesSpeech ={
     author:'Demosthenes',
     year:   342,
     BCE:    true,
     }
 
-///an array for the speeches///
+////an array for the speeches////
 
-var allSpeeches=[
+var allSpeeches =[
     churchillSpeech,
     ghandiSpeech,
     demosthenesSpeech
     ];
 
-///variable calculation///
+////variable calculation////
 
-var difference= 
+var difference = 
     ghandiSpeech.year - 
     churchillSpeech.year;
 
-///Console log statment ///
-console.log("Ghandi's & Churchill's speeches are " + difference + "years apart.");
+///Console log statment////
+console.log("Ghandi's & Churchill's speeches are "
+	+difference+ " years apart.");
+
 
 
 
