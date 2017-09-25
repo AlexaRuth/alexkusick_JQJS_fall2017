cf551d91ff605c20fbbbbfa56fc34ae06b355279
AlexaRuth <akusick@mcad.edu>
2017-09-24 21:07:59 -0500

    Updates / Details
    trying to figure out how to make the code look clean but read correctly
    in the console.

--- week4/script.js
+++ week4/script.js
@@ -1,29 +1,29 @@
 ////event listeners////
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  console.log("There are " 
-  	+allSpeeches.length +" speeches on this page.");
+  console.log(" There are " 
+  	+allSpeeches.length+" speeches on this page. ");
 	});
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
-  console.log("This speech was written by "
-	+allSpeeches[0].author +"in" 
-	+allSpeeches[0].year +". It is " 
-	+allSpeeches[0].BCE +"that this year is B.C.E.");
+  console.log(" This speech was written by "
+	+allSpeeches[0].author+" in " 
+	+allSpeeches[0].year+" . It is " 
+	+allSpeeches[0].BCE+" that this year is B.C.E. ");
 	});
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
-  console.log("This speech was written by " 
-  	+allSpeeches[1].author +"in" 
-  	+allSpeeches[1].year +". It is " 
-  	+allSpeeches[1].BCE +"that this year is B.C.E.");
+  console.log(" This speech was written by " 
+  	+allSpeeches[1].author+" in " 
+  	+allSpeeches[1].year+". It is " 
+  	+allSpeeches[1].BCE+" that this year is B.C.E. ");
 	});
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
-  console.log("This speech was written by " 
-  	+ allSpeeches[2].author +"in" 
-  	+ allSpeeches[2].year + ". It is " 
-  	+ allSpeeches[2].BCE + " that this year is B.C.E.");
+  console.log(" This speech was written by " 
+  	+allSpeeches[2].author+" in " 
+  	+allSpeeches[2].year+" . It is " 
+  	+allSpeeches[2].BCE+" that this year is B.C.E. ");
 	});
 
 
@@ -62,8 +62,8 @@ var difference =
     churchillSpeech.year;
 
 ///Console log statment////
-console.log("Ghandi's & Churchill's speeches are "
-	+difference+ " years apart.");
+console.log(" Ghandi's & Churchill's speeches are "
+	+difference+ " years apart. ");
 
 
 
