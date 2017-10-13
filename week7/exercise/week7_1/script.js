eaf373dded4491fe1d0e1bac637631c5e7113260
Alex Kusick <Akusick@mcad.edu>
2017-10-12 23:26:08 -0500

    week 7 assignment 2 updates

--- week7/exercise/week7_1/script.js
+++ week7/exercise/week7_1/script.js
@@ -1,11 +1,5 @@
-//<div id="textbody"> (this is my element grabber)
-//Var myNewElement = document.createElement (“p”); 
-//var myText = document.createTextNode("NEw list item text");
-//myNewElement.appendChild(MYTEXT);
-
 var newHeading = document.createElement("h1");
 var newParagraph = document.createElement("p");
-
 
 var h1Text = document.createTextNode("Welcome to week 7 its not DOS");
 var paraText = document.createTextNode("Learning what Document Object Model is.");
@@ -13,10 +7,13 @@ var paraText = document.createTextNode("Learning what Document Object Model is."
 newHeading.appendChild(h1Text);
 newParagraph.appendChild(paraText);
 
-//newHeading.innerHTML = "Did You know?";
-//newParagraph.innerHTML = "California procuces";
-
 document.getElementById("textbody").appendChild(newHeading);
 document.getElementById("textbody").appendChild(newParagraph);
 
 document.getElementById("textbody").style.textAlign = "center";
+
+//////NOTES/////
+//<div id="textbody"> (this is my element grabber)
+//Var myNewElement = document.createElement (“p”); 
+//var myText = document.createTextNode("NEw list item text");
+//myNewElement.appendChild(MYTEXT);
