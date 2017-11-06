a181b19927fcf9b1769c2cb4ddbe3f2bf67ef08d
Alex Kusick <Akusick@mcad.edu>
2017-11-05 21:01:41 -0600

    FINAL COMMIT

--- week10/js/script.js
+++ week10/js/script.js
@@ -3,10 +3,14 @@
 $(document).ready(function() {
 // all custom jQuery will go here
     
+//I THOUGHT THE PARAGRAPH WORKS BETTER FOR THIS THAN THE TITLE - I READ SOMEWHERE IN THE ASSIGNMENT... SOMETHING ABOUT A RECEPIE CARD? MAYBE THIS SECTION WOULD POTENTIALLY BE A RECIEPE? IF SO THEN EACH HEADER WOULD HAVE A SEPRATE TOGGLE EVENT RIGHT?... HMM CURRENTLY WHEN YOU CLICK THIS BOTH SECTIONS HIDE.
+    
     $( "h2.title" ).click(function() {
         $( ".entry" ).toggle( "slow" );
         });    
 
+//I THINK THIS COULD ALSO WORK AS A TOGGLE FUNCTION? BUT FOR SAKE OF COMPELTING THE ASSIGNMENT THE BEST WAY I CAN ILL LEAVE THE CLICK FUNCTION FOR IT//
+    
     $("a").click(function(){
         $(this).css("background", "aqua");
         });       
