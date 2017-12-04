96476163af6a892c03cf6a1cf357322d3b00103a
Alex Kusick <Akusick@mcad.edu>
2017-12-03 22:04:50 -0600

    updates

---
+++ week13/StateCaps2/js/script.js
@@ -0,0 +1,50 @@
+    
+// ALWAYS WRAP JQUERY IN THIS DOC READY FUNCTION //   
+
+$(document).ready(function(e){
+  
+    
+$('#stategallery').find('a').colorbox
+    ({ rel:'gal' });
+      });  
+    
+    
+    
+    
+/// $('#menu a').click(function(e){
+///    hideContentDivs();
+///    var tmp_div =
+///        $(this).parent().index();
+///    $('.main div').eq(tmp_div).show(
+//        800,"fade");
+///  });
+
+///function hideContentDivs(){
+///    $('.main div').each(function(){
+///        $(this).hide();});
+///}
+///    hideContentDivs(); 
+  
+    
+
+///*** CHANGE APPERANCE OF NAV ITEM ***/// 
+    
+///$( "li " ).hover(function() {
+///  $( this ).fadeOut( 100 );
+///  $( this ).fadeIn( 500 );
+///});
+    
+    
+    
+    
+///}); 
+
+//THIS IS THE END OF THE CURLY BRACKET FOR ONREADY FUNCTION CODE
+                             
+
+//6. TOGGLE h2 ELEMENT IN SIDEBAR  - MADE THIS ONLY WORK FOR ONE OF THE TWO PARAGRAPHS WHOOP!                            
+//$( "h2.title" ).click(function() {
+//   $( ".entry" ).toggle( "slow" );
+//});                            
+    
+
