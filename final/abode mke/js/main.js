253200b6cc6750f6904bfb97d5146a85f8c00d25
Alex Kusick <Akusick@mcad.edu>
2017-12-15 08:58:22 -0600

    commit

--- final/abode mke/js/main.js
+++ final/abode mke/js/main.js
@@ -1,9 +1,19 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
+                
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+$(document).ready(function() {
+    
+    $( "h2.title" ).click(function() {
+    $( ".entry" ).toggle show( "slow" );
+});
+    
+    
+    
+
+// 2. HEADER COLOR CHANGE 
+$('#header h1').click(function (){
+    $("#header").css("background","aquamarine");
+});
+
+  
+                                
+    }
